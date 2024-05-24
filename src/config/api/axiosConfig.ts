57591d11b99925a71243793c96d28d64732ff54f
PricedTuna <edgarevegar04@gmail.com>
2024-05-24 08:15:24 -0700

    Cambio a produccion

--- src/config/api/axiosConfig.ts
+++ src/config/api/axiosConfig.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 export const axiosApi = axios.create({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',  // Local
+  baseURL: 'https://banchidoback-production.up.railway.app',    // Produccion
   headers: { 
     'Content-Type': 'application/json'
   },
